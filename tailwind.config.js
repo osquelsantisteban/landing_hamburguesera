d712c5afde4ef2e91fd2f8088696cea3ddf4db6d
Osquel <77018049+osquelsantisteban@users.noreply.github.com>
2023-01-04 23:50:05 -0500

    add fonts and color palette

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+    content: ['./src/**/*.{html,js}'],
+    theme: {
+      fontFamily: {
+        'inter': ['inter'],
+        'coustard': ['coustard'],
+      },
+      extend: {
+        colors: {
+          'red-strong': '#e6332a',              
+        }
+      }
+    },
+  }
