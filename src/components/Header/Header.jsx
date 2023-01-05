609fa8aec450683ab528838d22aa63bea38f008e
Osquel <77018049+osquelsantisteban@users.noreply.github.com>
2023-01-05 16:12:44 -0500

    new change

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -19,7 +19,7 @@ export default function Header(){
             </section>
 
             <section className='relative'>
-                
+                <Carrusel />
             </section>
         </header>
     );
