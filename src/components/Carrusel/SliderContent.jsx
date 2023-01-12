c069073cff67b1fae03fcc3204339bd3805e2679
Osquel <77018049+osquelsantisteban@users.noreply.github.com>
2023-01-12 12:17:42 -0500

    change styles

--- src/components/Carrusel/SliderContent.jsx
+++ src/components/Carrusel/SliderContent.jsx
@@ -2,10 +2,10 @@ import React from 'react';
 
 export default function SliderContent({ activeIndex, imageSlider }) {
     return (
-        <div className=''>
+        <div className='grid items-center grid-cols-1 mx-0 justify-items-center'>
             {
                 imageSlider.map((slide,index) => 
-                    <figure key={index} className={ index === activeIndex ? 'slides active' : 'inactive'}>
+                    <figure key={index} className={ index === activeIndex ? 'relative mx-0 w-96 h-64 lg:w-11/12 lg:h-[800px]' : 'inactive'}>
                         <img src={slide.url} alt={slide.descripcion} className='slide-image'/>
                     </figure>
             )}
