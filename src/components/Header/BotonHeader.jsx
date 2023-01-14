6c3edf7a2a67b73f919ef44a7dd7568e663324a2
Osquel <77018049+osquelsantisteban@users.noreply.github.com>
2023-01-14 17:56:29 -0500

    upd

--- src/components/Header/BotonHeader.jsx
+++ src/components/Header/BotonHeader.jsx
@@ -1,7 +1,8 @@
 import React from 'react';
+import './Header.css';
 
 export default function BotonHeader(){
     return (
-        <button className='text-white uppercase border border-white h-[41px] w-[144px] rounded-3xl lg:text-base '>Pide Ahora</button>
+        <button className='text-white uppercase border border-red-strong h-[41px] w-[144px] rounded-3xl lg:text-base bordeDifuminado'>Pide Ahora</button>
     );
 }
