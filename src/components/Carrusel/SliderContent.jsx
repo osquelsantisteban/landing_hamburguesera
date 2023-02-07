7e39dbaf2eeca690bcface3799d3b5f2658f0593
Osquel <77018049+osquelsantisteban@users.noreply.github.com>
2023-02-06 23:01:06 -0500

    fix slider mobile views

--- src/components/Carrusel/SliderContent.jsx
+++ src/components/Carrusel/SliderContent.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 export default function SliderContent({ activeIndex, imageSlider }) {
     return (
-        <div className='grid items-center grid-cols-1 mx-0 justify-items-center'>
+        <div className='grid items-center grid-cols-1 mx-0 my-16 lg:my-0 justify-items-center'>
             {
                 imageSlider.map((slide,index) => 
                     <figure key={index} className={ index === activeIndex ? 'relative mx-0 w-96 h-64 lg:w-11/12 lg:h-[800px]' : 'inactive'}>
