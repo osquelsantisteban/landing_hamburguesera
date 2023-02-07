afd7fd63f6883cb67c5ba52fb3ac1f2d87398552
Osquel <77018049+osquelsantisteban@users.noreply.github.com>
2023-02-07 11:47:55 -0500

    add and delete image in to background

--- src/components/Background/Background.jsx
+++ src/components/Background/Background.jsx
@@ -1,8 +1,14 @@
 import React,{useEffect} from 'react';
-import backFoodMob from './assets/back-food-mobile.png';
+/* import backFoodMob from './assets/back-food-mobile.png';
 import leftFood from './assets/food-left.png';
-import rightFood from './assets/food-right.png';
-import Pizza from '../../assets/images/Pizza.png';
+import rightFood from './assets/food-right.png'; */
+// import Pizza from '../../assets/images/Pizza.png';
+import Burguer1 from './assets/Burguer SD 1.png';
+import Burguer2 from './assets/Burguer SD 2.png';
+import Pizza1 from './assets/Pizza ID 1.png';
+import Pizza2 from './assets/Pizza ID 2.png';
+import Pizza3 from './assets/Pizza SI.png';
+import Vaso from './assets/Vaso.png';
 
 export function BackFood() {
     
@@ -29,22 +35,29 @@ export function BackFood() {
 
 
     const isMobile = (window.innerWidth <= 648)
-    return <>
-        <section className='fixed flex w-full bg-transparent z-20 pt-14'>
-                
-                <div className='relative overflow-hidden flex-1 w-full flex h-screen justify-center items-center'>
-                    <img src={Pizza} alt="" className='moueseEffect object-contain' data-value="-5" />
-                    <img src={Pizza} alt="" className='moueseEffect object-contain' data-value="5" />
-                    <img src={Pizza} alt="" className='moueseEffect object-contain' data-value="19" />
+    return <section className='fixed h-screen w-screen bg-transparent z-20 pt-14 overflow-hidden'>                                    
+            {
+                isMobile ? 
+                <div className='relative w-2/4 overflow-hidden flex-1 flex flex-wrap h-screen justify-center items-center object-contain'>
+                    <img src={Vaso} alt="" className='moueseEffect object-contain' data-value="-5" />                                
+                </div>                            
+                : 
+                <div className='relative w-full overflow-hidden flex-1 flex flex-wrap h-screen justify-center items-center object-contain'>
+                    <img src={Burguer2} alt="" className='absolute moueseEffect object-contain' data-value="5" />
+                    <img src={Burguer1} alt="" className='absolute moueseEffect object-contain' data-value="-5" />
+                    <img src={Pizza1}   alt="" className='absolute moueseEffect object-contain' data-value="-3" />
+                    <img src={Pizza2}   alt="" className='absolute moueseEffect object-contain' data-value="4" />
+                    <img src={Pizza3}   alt="" className='absolute moueseEffect object-contain' data-value="10" />
+                    <img src={Vaso}     alt="" className='absolute moueseEffect object-contain' data-value="19" />
                 </div>
+            }
 
         </section>
-    </>
 }
 
 export function Background(){    
     return (
-        <section className={`fixed w-full md:bg-back bg-back-mobile h-[100vh] z-0 pt-14`}>
+        <section className={`fixed w-full md:bg-back bg-cover bg-back-mobile h-[250vh] z-0 pt-14 overflow-hidden bg-repeat-y`}>
         </section>
     );
 }
