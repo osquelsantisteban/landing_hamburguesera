58249f604671200ee476d0b973018a9162404d8f
Osquel <77018049+osquelsantisteban@users.noreply.github.com>
2023-02-06 22:53:39 -0500

    remove opcacity in header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -4,8 +4,8 @@ import BotonHeader from './BotonHeader';
 
 export default function Header() {
     return (
-        <header className='fixed w-full z-40'>{/* shadow-[0_20px_40px_3px_#000] */}
-            <section className='flex h-16 bg-gradient-to-b from-black to-black/5 items-center'>
+        <header className='fixed w-full z-40 shadow-[0_20px_40px_3px_#000]'>
+            <section className='flex h-16 bg-black items-center'>
                 <div className='flex items-center justify-between flex-1 mx-2 md:mx-6 lg:mx-24'>
                     
                     <div className=''>
@@ -14,7 +14,6 @@ export default function Header() {
 
                     <BotonHeader />
                 </div>
-
             </section>
         </header>
     );
