332498a3b1b1ce730ac46809092362d908ea38b0
Osquel <77018049+osquelsantisteban@users.noreply.github.com>
2023-02-22 09:53:30 -0500

    Fix svg arrows

--- src/components/Carrusel/Arrows.jsx
+++ src/components/Carrusel/Arrows.jsx
@@ -4,10 +4,14 @@ export default function Arrows({prevSlide,nextSlide}){
     return(
         <div className='arrows'>
             <span className='prev' onClick={prevSlide}>
-                <svg xmlns="http://www.w3.org/2000/svg" className='w-4 lg:w-6' viewBox="99.8 399.133 25.4 27.067"><path d="m13.333 0 13.334 25H0z" strokeLinejoin="round" strokeLinecap="round" stroke="#fff" fill="transparent" transform="matrix(0 -.9625 .96 0 100.5 425.5)" dataname="prev"/></svg>
+                <svg className='w-4 lg:w-6' viewBox="0 0 21.63 22.67">
+                    <path fill="#fff" d="M1,11.34A2,2,0,0,0,2.06,13.1l15.62,8.34a2,2,0,0,0,2.95-1.77V3a2,2,0,0,0-2.95-1.76L2.06,9.57A2,2,0,0,0,1,11.34m-1,0A3,3,0,0,1,1.59,8.69L17.21.36A3,3,0,0,1,21.63,3V19.67a3,3,0,0,1-4.42,2.65L1.59,14A3,3,0,0,1,0,11.34Z"/>
+                </svg>
             </span>
             <span className='next' onClick={nextSlide}>
-                <svg xmlns="http://www.w3.org/2000/svg" className='w-4 lg:w-6' viewBox="1797.8 426.133 25.4 27.067"><path d="m13.333 0 13.334 25H0z" strokeLinejoin="round" strokeLinecap="round" stroke="#fff" fill="transparent" transform="matrix(0 .9625 -.96 0 1822.5 426.833)" dataname="next"/></svg>
+                <svg className='w-4 lg:w-6' viewBox="0 0 21.63 22.67">
+                    <path fill="#fff" d="M20.63,11.34a2,2,0,0,0-1.06-1.77L3.94,1.24A2,2,0,0,0,1,3V19.67a2,2,0,0,0,2.94,1.77L19.57,13.1a2,2,0,0,0,1.06-1.76m1,0A3,3,0,0,1,20,14L4.41,22.32A3,3,0,0,1,0,19.67V3A3,3,0,0,1,4.41.36L20,8.69A3,3,0,0,1,21.63,11.34Z"/>
+                </svg>
             </span>
         </div>
     );
